902e3d1a8f4256472a0131e0dbec174743e40164
saadahmadhere <52596800+saadahmadhere@users.noreply.github.com>
2021-05-31 06:16:13 +0530

    done lecture 10

--- src/Home.js
+++ src/Home.js
@@ -2,17 +2,23 @@ import {useState} from 'react'
 
 const Home = () =>{
 
-    var [greeting,setGreeting] = useState('sir')
-
-    const clickHandler = (e) =>{
-        setGreeting('mam')
-    }
+    const [item,setItem] = useState([
+        { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
+        { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
+        { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
+    ]
+    )
 
     return(
         <div className="home">
-            <h2>Homepage</h2>
-            <p>Hello {greeting}</p>
-            <button onClick = {clickHandler}>Click me</button>
+           {item.map((blog) =>(
+               <div className="blog-preview">
+                   <h2>{blog.title}</h2>
+                   <p> Written by: {blog.author}</p>
+                   <hr />
+               </div>
+               
+           ))}
         </div>
     );
 }
