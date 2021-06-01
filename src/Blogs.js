ea5f651f95e5492208eb0865ab30704fcf0cbb18
saadahmadhere <52596800+saadahmadhere@users.noreply.github.com>
2021-06-01 07:01:18 +0530

    11 correct

--- src/Blogs.js
+++ src/Blogs.js
@@ -1,12 +1,14 @@
 const Blogs = (props) =>{
     return(
         <div className="blog-page">
-             {props.item.map((blog) =>(
+             {props.blogs.map((blog) =>(
+            <div>
                <div className="blog-preview">
                    <h2>{blog.title}</h2>
                    <p> Written by: {blog.author}</p>
-                   <hr />
                </div>
+                <hr />
+            </div>
            ))}
         </div>
     );
--- src/Home.js
+++ src/Home.js
@@ -3,7 +3,7 @@ import Blogs from './Blogs'
 
 const Home = () =>{
 
-    const [item,setItem] = useState([
+    const [blogs,setBlogs] = useState([
         { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
         { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
         { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
@@ -12,7 +12,7 @@ const Home = () =>{
 
     return(
         <div className="home">
-          <Blogs item = {item}/>
+          <Blogs blogs ={blogs}/>
         </div>
     );
 }
