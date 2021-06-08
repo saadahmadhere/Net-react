7f7ef9e7b9872370f48ed5f5c897a2dbdd309490
saadahmadhere <52596800+saadahmadhere@users.noreply.github.com>
2021-06-08 13:03:55 +0530

    19 Handling fetch errors

--- src/Home.js
+++ src/Home.js
@@ -5,24 +5,33 @@ const Home = () =>{
 
     const [blogs,setBlogs] = useState(null)
     const[isPending, setIsPending] = useState(true);
+    const[error, setError] = useState(null);
 
     useEffect(()=>{
       setTimeout(()=>{
         fetch("http://localhost:8000/blogs/")
         .then(respose =>{
+            // console.log(respose)
+            if(!respose.ok){
+                throw Error; 
+            }
             return respose.json();
         }).then((data) =>{
             // console.log(data)
             setBlogs(data)
             setIsPending(false)
+            setError(null)
         }).catch((err)=>{
-            console.log(err)
+            console.log(err.message)
+            setError(true)
+            setIsPending(false)
         })
       },1000)   //the timeout is set only for understanding concept. Don't forget to reemove it.
     },[])
 
     return(
         <div className="home">
+        {error && <h1>error message</h1>}
         {(isPending) && <h1>Loading...</h1> }
         {blogs &&  <Blogs blogs ={blogs} title = "All blogs" />}
         </div>
