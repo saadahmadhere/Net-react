c3fc0e3ce3b2bab286a9736adc2610de957b5d66
saadahmadhere <52596800+saadahmadhere@users.noreply.github.com>
2021-06-11 11:33:36 +0530

    26- reusing custom hooks

--- src/BlogDetails.js
+++ src/BlogDetails.js
@@ -1,11 +1,21 @@
 import { useParams } from "react-router";
+import useFetch from "./useFetch";
 
 const BlogDetails = () => {
 
-const {id} = useParams(); //This helps to get the route parameter.useParams returns an object of key/value pairs of URL parameters. Use it to access match.params of the current <Route>
+    const {id} = useParams(); //This helps to get the route parameter.useParams returns an object of key/value pairs of URL parameters. Use it to access match.params of the current <Route>
+    const {item: blog, isPending, error} = useFetch("http://localhost:8000/blogs/"+id)
     return ( 
         <div className="blog-details">
-            <h1> Blog Details- {id}</h1>
+            {isPending && <h1> Loading...</h1>}
+            {error && <h1>Error Message</h1>}
+            {blog && (
+                <article>
+                    <h2>{blog.title}</h2>
+                    <p>{blog.author}</p>
+                    <div>{blog.body}</div>
+                </article>
+            )}
         </div>
      );
 }
