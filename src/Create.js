499a8473efc31f770fe0dee2b7b6f0b01ece617d
saadahmadhere <52596800+saadahmadhere@users.noreply.github.com>
2021-06-13 14:20:37 +0530

    29- making a post request

--- src/Create.js
+++ src/Create.js
@@ -1,7 +1,52 @@
+import { useState } from "react";
+
 const CreateBlog = () => {
+
+    const[title,setTitle] = useState('')
+    const[body, setBody] = useState('')
+    const[author, setAuthor] = useState('mario')
+    const[isPending, setIsPending] = useState(false)
+
+    const handleSubmit = (event) =>{
+        event.preventDefault();
+        const blog = {title,body,author}
+        setIsPending(true)
+
+        fetch('http://localhost:8000/blogs/',{
+            method: 'POST',
+            headers: {'Content-Type':'application/json'}, //this shows what type of content is being sent with the request.
+            body: JSON.stringify(blog)
+        }).then(()=>{
+            setIsPending(false)
+        })
+    }
     return(
-        <div>
+        <div className='create'>
             <h1> Create your blog here.</h1>
+            <form onSubmit = {handleSubmit}>
+                <label>Blog title:</label>
+                <input type="text"
+                value = {title}
+                onChange ={(event) => setTitle(event.target.value) }
+                required 
+                autoFocus
+                />
+                <label>Body:</label>
+                <textarea 
+                value = {body}
+                onChange = {(event)=> setBody(event.target.value)}
+                required />
+                <label>Author:</label>
+                <select 
+                value = {author}
+                onChange = {(event)=> setAuthor(event.target.author)}
+                >
+                    <option value="mario">Mario</option>
+                    <option value="luigi">Luigi</option>
+                </select>
+                {!isPending && <button>Add blog</button>}
+                {isPending && <button disabled>Adding blog...</button>}
+            </form>
         </div>
     )
 }
